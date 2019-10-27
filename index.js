aba80b10f1ab38647e440314230b769afc84c407
Abdullah Al Jahid <abdullah.al.jahid@gmail.com>
2019-10-28 01:09:34 +0600

    Export bug fix

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import { NativeModules, requireNativeComponent } from 'react-native';
 
-const { CrispChat } = NativeModules;
+export const { CrispChat } = NativeModules;
 export const CrispChatWindow = requireNativeComponent('RCTCrispChatWindow');
-export const CrispChat = CrispChat;
